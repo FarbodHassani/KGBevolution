8f269cdfb45a9533a7253d6c5c55f7f89522b9a6
FarbodHassani <farbod.hassani@gmail.com>
2021-03-19 14:24:01 +0100

    Some mathematica notebooks to find the eom
    This is a version which works, based on old Emilio's package

---
+++ Mathematica/EQscalarpert.m
@@ -0,0 +1,122 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Listify[SVTExpand, 
+ (Listify[SVTDecomposition, -2*metricg[\[Gamma]$144310, \[Gamma]$144311]*
+      CD[-\[Gamma]$144311][CD[-\[Gamma]$144310][pertscalarcov[LI[1]]]]*
+      Derivative[0, 1][Kfun][Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] - 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144311]*CD[-\[Gamma]$144311][
+       CD[-\[Gamma]$144310][scalarcov[]]]*Derivative[0, 1][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][pertscalarcov[LI[1]]]*
+      CD[-\[Gamma]$144311][pertscalarcov[LI[1]]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][pertscalarcov[LI[1]]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     4*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][pertscalarcov[LI[1]]]*
+      CD[-\[Gamma]$144311][scalarcov[]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][pertscalarcov[LI[1]]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][scalarcov[]]*
+      CD[-\[Gamma]$144311][scalarcov[]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][pertscalarcov[LI[1]]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][pertscalarcov[LI[1]]]*
+      CD[-\[Gamma]$144311][pertscalarcov[LI[1]]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][scalarcov[]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     4*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][pertscalarcov[LI[1]]]*
+      CD[-\[Gamma]$144311][scalarcov[]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][scalarcov[]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] + 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144312]*metricg[\[Gamma]$144311, 
+       \[Gamma]$144313]*CD[-\[Gamma]$144310][scalarcov[]]*
+      CD[-\[Gamma]$144311][scalarcov[]]*CD[-\[Gamma]$144313][
+       CD[-\[Gamma]$144312][scalarcov[]]]*Derivative[0, 2][Kfun][
+       Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] - 
+     2*Derivative[1, 0][Kfun][Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] - 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144311]*CD[-\[Gamma]$144310][
+       pertscalarcov[LI[1]]]*CD[-\[Gamma]$144311][pertscalarcov[LI[1]]]*
+      Derivative[1, 1][Kfun][Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] - 
+     4*metricg[\[Gamma]$144310, \[Gamma]$144311]*CD[-\[Gamma]$144310][
+       pertscalarcov[LI[1]]]*CD[-\[Gamma]$144311][scalarcov[]]*
+      Derivative[1, 1][Kfun][Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2] - 
+     2*metricg[\[Gamma]$144310, \[Gamma]$144311]*CD[-\[Gamma]$144310][
+       scalarcov[]]*CD[-\[Gamma]$144311][scalarcov[]]*
+      Derivative[1, 1][Kfun][Scalar[pertscalarcov[LI[1]]] + scalarcov[], 
+       (-Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][pertscalarcov[LI[1]]]] - 
+         2*Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][pertscalarcov[
+              LI[1]]]*CD[-\[Beta]][scalarcov[]]] - 
+         Scalar[metricg[\[Beta], \[Alpha]]*CD[-\[Alpha]][scalarcov[]]*
+           CD[-\[Beta]][scalarcov[]]])/2], {0, {}}, ListMethod -> "Part", 
+    PartLength -> 50, Verbose -> True]*scale[]^2)/4, {}, 
+ ListMethod -> "SamePerts", PartLength -> 50, Verbose -> True]
